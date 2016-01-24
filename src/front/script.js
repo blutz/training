34fe237b98abbdbca1f68435f2b8b681c1e22b95
Byron Lutz <byronlutz@gmail.com>
2016-01-23 23:55:22 -0800

    Made the first react component

--- src/front/script.js
+++ src/front/script.js
@@ -3,7 +3,19 @@ var ReactDOM = require('react-dom');
 
 var reactMount = document.getElementById('react-mount');
 
+var Container = React.createClass({
+  render: function() {
+    return (
+      <div className="row">
+        <div className="small-12 columns">
+          <h2>Hello from inside foundation</h2>
+        </div>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <Container />,
   reactMount
 );
